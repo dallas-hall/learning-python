c6d4aebcd2efab037eafd905c1fddccff9391005
Dallas Hall <mr.dallas.hall@gmail.com>
2017-11-15 09:17:47 +1100

    Initial upload

---
+++ Automate The Boring Stuff/Chapter02/importing modules - PRN module.py
@@ -0,0 +1,7 @@
+#inport the random module
+import random
+
+#loop 10 times
+for i in range(10):
+    #print a random int between 1 and 5 (both are inclusive)
+    print(random.randint(1, 5))
---
+++ Automate The Boring Stuff/Chapter02/multiple argument for loop.py
@@ -0,0 +1,10 @@
+# first argument is the start value (min)
+# second argument is the value to stop at, but that value is excluded (max - 1)
+for i in range(8, 16) :
+    print(i)
+
+# first argument is the start value (min)
+# second argument is the value to stop at, but that value is excluded (max - 1)
+# third arugment is how much to increment by
+for i in range(8, 16, 2) :
+    print(i)
