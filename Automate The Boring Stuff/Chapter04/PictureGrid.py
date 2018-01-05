4c9ffcf05f7c6471265246c9ec35dd837d2e2d50
Dallas Hall <dallas.hall@example.com>
2018-01-05 16:29:55 +1100

    Printing picture as is, 90 degrees to the right, 90 degrees to the left, and 180 degrees.

--- Automate The Boring Stuff/Chapter04/PictureGrid.py
+++ Automate The Boring Stuff/Chapter04/PictureGrid.py
@@ -16,23 +16,25 @@ print('Columns:\t' + str(len(grid[1])) + '\n')  # 6 across
 
 # @@@ PRINT PICTURE AS IS @@@
 # i starts at 0, increments by 1 until the entire array is traversed
+print('@@@ PRINT PICTURE AS IS @@@\n')
 for i in range(len(grid)):
 	output = ''
-	# j starts at 0, increments by 1 until the entire nested array is traversed
+	# j starts at 0, increments by 1 until the end of the nested array (6)
 	for j in range(len(grid[i])):
 		# grab the value from the 2D array
 		output += grid[i][j]
-	# add a new line each time the entire nested array is traversed
+	# add a new line each time the loop is over
 	output += '\n'
 	print(output)
 print('\n')
 
 # @@@ ROTATE PICTURE 90 DEGREES TO THE RIGHT @@@
 # ### First attempt ###
-# i starts at 0, increments by 1 until the end of the nested array
+print('@@@ ROTATE PICTURE 90 DEGREES TO THE RIGHT @@@\n')
+# i starts at 0, increments by 1 until the end of the nested array (6)
 for i in range(len(grid[1])):
 	output = ''
-	# j starts at 8, decrements by 1, until it hits 0
+	# j starts at 8, decrements by 1, , until the start of the nested array (0)
 	for j in range(8, -1, -1):
 		# grab the value from the 2D array
 		# output += 'j=' + str(j) + ' & i=' + str(i) + '. '
@@ -42,8 +44,21 @@ for i in range(len(grid[1])):
 	print(output)
 print('\n')
 
+# ### Second Attempt ###
+# i starts at 0, increments by 1, until the end of the nested array (6)
+for i in range(len(grid[i])):
+	output = ''
+	# j is 0, increments by 1, until the end of the array (9)
+	for j in range(len(grid)):
+		# output += 'j=' + str(j) + ' & i=' + str(i) + '. '
+		output += grid[j][i]
+	output += '\n'
+	print(output)
+print('\n')
+
 # @@@ ROTATE PICTURE 90 DEGREES TO THE LEFT @@@
 # ### First attempt ###
+print('@@@ ROTATE PICTURE 90 DEGREES TO THE LEFT @@@\n')
 # i starts at 5, decrements by 1, until it hits 0
 for i in range(5, -1, -1):
 	output = ''
@@ -53,6 +68,20 @@ for i in range(5, -1, -1):
 		# output += 'i=' + str(i) + ' & j=' + str(j) + '. '
 		output += grid[j][i]
 	# add a new line each time the entire nested array is traversed
+	output += '\n'
+	print(output)
+print('\n')
+
+# @@@ ROTATE PICTURE 180 DEGREES @@@
+# ### FIRST ATTEMPT ###
+print('@@@ ROTATE PICTURE 180 DEGREES @@@\n')
+# i starts at 8, decrements by 1, until the start of the array (0)
+for i in range(8, -1, -1):
+	output = ''
+	# j starts at 5, decrements by 1, until the start of the nested array (0)
+	for j in range(5, -1, -1):
+		#output += 'i=' + str(i) + ' & j=' + str(j) + '. '
+		output += grid[i][j]
 	output += '\n'
 	print(output)
 print('\n')
