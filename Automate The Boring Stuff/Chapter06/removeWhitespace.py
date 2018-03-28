f42e1a5b30ee61bfe329be19e8e57d7fb3f2d47c
Dallas Hall <mr.dallas.hall@gmail.com>
2018-03-28 15:56:48 +1100

    Even MOAR string manipulation code.

---
+++ Automate The Boring Stuff/Chapter06/removeWhitespace.py
@@ -0,0 +1,5 @@
+message = '\t   Hello world \t  '
+print('Original: \'' + message + '\'')
+print('Remove all spaces: \'' + message.strip() + '\'')
+print('Remove all left sided spaces: \'' + message.lstrip() + '\'')
+print('Remove all right sided spaces: \'' + message.rstrip() + '\'')
