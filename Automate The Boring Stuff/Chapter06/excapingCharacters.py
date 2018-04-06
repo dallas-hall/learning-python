02ea2991bed509bec8c7172d4bb20430cb3ce1cb
Dallas Hall <dallas.hall@example.com>
2018-04-06 16:23:26 +1000

    Escaping example, pyperclip (cut and paste) demo, and crappy password manager project.

---
+++ Automate The Boring Stuff/Chapter06/excapingCharacters.py
@@ -0,0 +1,8 @@
+print('Printing \'single quotes\'')
+print("Printing 'single quotes'")
+print("Printing \"double quotes\"")
+print('Printing "double quotes"')
+print(r'\t\n\'')
+print('''Hello
+	there
+	sire.''')
