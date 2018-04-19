108f792207a465997c74c3c6d5f2ab099067bb8f
Dallas Hall <mr.dallas.hall@gmail.com>
2018-04-19 16:04:35 +1000

    Added moar fancy stuff.

--- Automate The Boring Stuff/Chapter07/FindUSPhoneNumberRegexWithGroups.py
+++ Automate The Boring Stuff/Chapter07/FindUSPhoneNumberRegexWithGroups.py
@@ -6,6 +6,10 @@ def is_us_phone_number(text):
 	match = us_phone_number.search(text)
 	matched = False
 	if match:
+		# matchObject.groups() returns a tuple, use the multi-assignment trick to pass them to variables.
+		area_code, phone_number = match.groups()
+		print('The area code is ' + area_code + ' & the number is ' + phone_number)
+
 		matched = True
 		# need + 1, so i will be less than last index + 1
 		for i in range(match.lastindex + 1):
@@ -18,7 +22,7 @@ def is_us_phone_number(text):
 			print('Found with match() ' + match.group(i))
 	return matched
 
-print('415-555-4242 is a phone number?')
+print('415-555-4242 is a US phone number?')
 print(is_us_phone_number('415-555-4242'))
-print('Moshi moshi is a phone number?')
+print('\nMoshi moshi is a US phone number?')
 print(is_us_phone_number('Moshi moshi'))
