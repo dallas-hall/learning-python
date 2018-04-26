b181d8f7f236bb98b656b77b7bd1d1f918d436f9
Dallas Hall <mr.dallas.hall@gmail.com>
2018-04-26 16:01:10 +1000

    Showing the difference between search() and match()

--- Automate The Boring Stuff/Chapter07/CaptureGroupsWithPipeOrLogic.py
+++ Automate The Boring Stuff/Chapter07/CaptureGroupsWithPipeOrLogic.py
@@ -2,6 +2,7 @@ import re
 
 # Use a capture group and or logic to search for a specific hero ending with man. Only the first is matched.
 regex = re.compile(r'(Bat|Super|Spider)man')
+print('@@@ match() @@@\nThis will only return a match if the start of the string matches the expression.')
 match = regex.match("Batman, Superman & Spiderman walk into a bar...")
 print(match.group() + ' says to barman...')
 match = regex.match("Superman, Batman & Spiderman walk into a bar...")
@@ -9,6 +10,7 @@ print(match.group() + ' says to barman...')
 match = regex.match("Spiderman, Superman & Batman walk into a bar...")
 print(match.group() + ' says to barman...')
 
+print('\n@@@ search() @@@\nThis will only return the first match of the pattern within the entire string.')
 match = regex.search("Batman, Superman & Spiderman walk into a bar...")
 print(match.group() + ' says to barman...')
 match = regex.search("Superman, Batman & Spiderman walk into a bar...")
