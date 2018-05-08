312611198985ad63ad84113c2bf3f7fc63657fe4
Dallas Hall <mr.dallas.hall@gmail.com>
2018-05-08 15:34:18 +1000

    Showing anchors.

---
+++ Automate The Boring Stuff/Chapter07-Regex/Anchors.py
@@ -0,0 +1,6 @@
+print('@@@ Shorthand Character Classes @@@')
+print('[INFO] This can be seen at https://www.regular-expressions.info/anchors.html')
+print('^ matches the position before the first character in the string.')
+print('$ matches the position after the last character in the string.')
+print('multi-line mode will make these anchors work with newlines as well as their original meanings.')
+
