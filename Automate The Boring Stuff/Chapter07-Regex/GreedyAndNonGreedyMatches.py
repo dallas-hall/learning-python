b4e4b510ac4a84d4065d3f5360bba55735d55792
Dallas Hall <dallas.hall@example.com>
2018-05-08 15:25:49 +1000

    Added greedy and non-greedy examples for dot star.

--- Automate The Boring Stuff/Chapter07-Regex/GreedyAndNonGreedyMatches.py
+++ Automate The Boring Stuff/Chapter07-Regex/GreedyAndNonGreedyMatches.py
@@ -2,6 +2,7 @@ import re
 
 print('@@@ Start String @@@')
 start_string = 'hahahahaha'
+html = '<p>Some text in a <i>pargraph</i>.</p>'
 print(start_string)
 
 print('\n@@@ Greedy Matching @@@')
@@ -12,6 +13,9 @@ print(regex.pattern + ' found ' + match_object.group())
 regex = re.compile(r'(ha){,5}')
 match_object = regex.search(start_string)
 print(regex.pattern + ' found ' + match_object.group())
+regex = re.compile(r'<.*>')
+match_object = regex.search(html)
+print(regex.pattern + ' found ' + match_object.group())
 
 print('\n@@@ Non-Greedy Matching @@@')
 regex = re.compile(r'(ha){3,5}?')
@@ -19,4 +23,7 @@ match_object = regex.search(start_string)
 print(regex.pattern + ' found ' + match_object.group())
 regex = re.compile(r'(ha){3,}?')
 match_object = regex.search(start_string)
+print(regex.pattern + ' found ' + match_object.group())
+regex = re.compile(r'<.*?>')
+match_object = regex.search(html)
 print(regex.pattern + ' found ' + match_object.group())
