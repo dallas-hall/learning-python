da32fd47fc79d2ee02e02b32dfca0af3811cc055
Dallas Hall <dallas.hall@example.com>
2018-05-09 16:07:09 +1000

    Comments and free spacing example.

---
+++ Automate The Boring Stuff/Chapter07-Regex/RegexCommentsAndSpacing.py
@@ -0,0 +1,24 @@
+import re
+print('@@@ Regex Comments Using Verbose Mode @@@')
+print('[INFO] This can be seen at https://www.regular-expressions.info/freespacing.html')
+print('This can be helpful when creating long and complex regex\'s. re.VERBOSE is needed to do this. Optionally \'\'\' can be used for multiple lines.')
+print('Complex regex with no comments or free spacing.')
+phoneRegex = re.compile(r'((\d{3}|\(\d{3}\))?(\s|-|\.)?\d{3}(\s|-|\.)\d{4}(\s*(ext|x|ext.)\s*\d{2,5})?)')
+print(str(phoneRegex))
+print('Complex regex with comments and free spacing.')
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?            # area code
+    (\s|-|\.)?                    # separator
+    \d{3}                         # first 3 digits
+    (\s|-|\.)                     # separator
+    \d{4}                         # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?  # extension
+    )''', re.VERBOSE)
+print(r'''(
+    (\d{3}|\(\d{3}\))?            # area code
+    (\s|-|\.)?                    # separator
+    \d{3}                         # first 3 digits
+    (\s|-|\.)                     # separator
+    \d{4}                         # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?  # extension
+)''')
