ad56df5f0cc6ceebfe9bc3e8552b69782c20c132
Dallas Hall <dallas.hall@example.com>
2018-05-09 15:49:44 +1000

    Case insensitive matching examples.

--- Automate The Boring Stuff/Chapter07-Regex/CaseInsensitiveMatching.py
+++ Automate The Boring Stuff/Chapter07-Regex/CaseInsensitiveMatching.py
@@ -1,3 +1,3 @@
 print('@@@ Case Insensitive Matching @@@')
 print('[INFO] This can be seen at https://www.regular-expressions.info/modifiers.html')
-print('To do this, you need to pass re.I as a second argument to re.compile')
+print('To do this, you need to pass re.IGNORECASE or re.I as a second argument to re.compile')
