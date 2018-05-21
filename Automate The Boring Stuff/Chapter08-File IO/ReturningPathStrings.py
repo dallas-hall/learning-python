9eafb92f828f24e28b16522e5fe62533aeae4b36
Dallas Hall <mr.dallas.hall@gmail.com>
2018-05-21 16:11:22 +1000

    Added an additional way to return a path and file from a string.

--- Automate The Boring Stuff/Chapter08-File IO/ReturningPathStrings.py
+++ Automate The Boring Stuff/Chapter08-File IO/ReturningPathStrings.py
@@ -1,7 +1,12 @@
 import os
 
-print('The current working directory is:\n' + os.getcwd())
-print('\nPath without file returned via os.path.dirname() is:\n' + os.path.dirname(os.getcwd()))
-print('\nPath with file returned via os.path.dirname() is:\n' + os.path.dirname(os.getcwd() + '/OSPathSupport.py'))
-print('\nPath without file returned via os.path.basename() is:\n' + os.path.basename(os.getcwd()))
-print('\nPath with file returned via os.path.basename() is:\n' + os.path.basename(os.getcwd() + '/OSPathSupport.py'))
+path_only = str(os.getcwd())
+path_and_file = str(os.getcwd() + '/OSPathSupport.py')
+
+print('The current working directory is:\n' + path_only)
+print('\nPath without file returned via os.path.dirname() is:\n' + os.path.dirname(path_only))
+print('\nPath with file returned via os.path.dirname() is:\n' + os.path.dirname(path_and_file))
+print('\nPath without file returned via os.path.basename() is:\n' + os.path.basename(path_only))
+print('\nPath with file returned via os.path.basename() is:\n' + os.path.basename(path_and_file))
+print('\nPath without file returned via os.path.split() is:\n' + str(os.path.split(path_only)))
+print('\nPath with file returned via os.path.split() is:\n' + str(os.path.split(path_and_file)))
