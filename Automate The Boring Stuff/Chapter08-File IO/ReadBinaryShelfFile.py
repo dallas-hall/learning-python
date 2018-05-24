080add0a185f7c836cf13b4802f588b81b70e2ce
Dallas Hall <dallas.hall@example.com>
2018-05-24 16:03:16 +1000

    Reading from a binary file and its map data structure. Added printing keys and values.

--- Automate The Boring Stuff/Chapter08-File IO/ReadBinaryShelfFile.py
+++ Automate The Boring Stuff/Chapter08-File IO/ReadBinaryShelfFile.py
@@ -3,6 +3,9 @@ path = '/home/dhall/tmp'
 filename = 'shelf-file.bin'
 # Open the file
 file = shelve.open(path + '/' + filename)
+# Print the keys and value pairs, need to convert to a list first.
+print(list(file.keys()))
+print(list(file.values()))
 # Read from the file using a map data structure
 my_list = file.get('my_list')
 file.close()
