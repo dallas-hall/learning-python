f02c4ac386f9ce1d81ce6121c2e737e0f66e03a7
Dallas Hall <dallas.hall@example.com>
2018-05-24 15:52:48 +1000

    Opening a file, and then writing / appending to the file. Writing = truncate existing file or create a new file. Appending = append to an existing file or create a new file.
    Added an additional read only open and content printing.

--- Automate The Boring Stuff/Chapter08-File IO/WriteToAFile.py
+++ Automate The Boring Stuff/Chapter08-File IO/WriteToAFile.py
@@ -4,9 +4,17 @@ file = open(file_path, 'w')
 # Need to specify end of line or nothing is written.
 file.write('Hello from python.\n')
 file.write('I am line two.')
+file.close()
 
 # Open in append mode, will append to an existing file or create a new file.
 file = open(file_path, 'a')
 # Need to specify end of line or nothing is written.
 file.write('This is the new text that doesn\'t delete the old text. I don\'t have a newline at the start so I am concatenated to the previous line.\n')
 file.write('I am another line! yo.')
+file.close()
+
+# Open the file in read mode and get all of its content.
+file = open(file_path, 'r')
+file_content = file.read()
+print(file_content)
+file.close()
