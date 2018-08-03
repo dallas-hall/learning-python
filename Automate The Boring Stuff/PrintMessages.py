585539889ef4867dcfdfcce6aca17d0a227b71d0
Dallas Hall <mr.dallas.hall@gmail.com>
2018-08-03 13:14:31 +1000

    Sick of writing the same logging messages. Wrote this, can use with: from PrintMessages import print_info, print_error, print_warning, print_debug

---
+++ Automate The Boring Stuff/PrintMessages.py
@@ -0,0 +1,15 @@
+#!/bin/python
+def print_info(message):
+	print('[INFO] ' + message)
+
+
+def print_warning(message):
+	print('[WARNING] ' + message)
+
+
+def print_error(message):
+	print('[ERROR] ' + message)
+
+
+def print_debug(message):
+	print('[DEBUG] ' + message)
