efebf25ab6261f70e0a5326331efbf0e8c7fb86d
Dallas Hall <dallas.hall@example.com>
2018-08-06 15:50:27 +1000

    Directory existence and creation functions created.

--- Automate The Boring Stuff/Chapter09-Organising Files/FillFileGaps.py
+++ Automate The Boring Stuff/Chapter09-Organising Files/FillFileGaps.py
@@ -1,38 +1,67 @@
-import os
+import os, shutil
+from pathlib import Path
 from PrintMessages import print_debug, print_info, print_error, print_warning
 
 
 def create_files_full(path, amount):
+	if not check_folder(path):
+		create_folder(path)
+		os.chdir(os.getcwd() + '/' + path)
 	for i in range(1, amount + 1):
 		filename = 'tmp_file' + str(i)
 		print_debug('FULL' + path + '/' + filename)
-		# current_file = open(path, 'w')
-		# current_file.write(path)
-		# current_file.close()
+		current_file = open(filename, 'w')
+		current_file.write(path + '/' + filename)
+		current_file.close()
 
 
 def create_files_even(path, amount):
+	if not check_folder(path):
+		create_folder(path)
+		os.chdir(os.getcwd() + '/' + path)
 	for i in range(1, amount + 1):
 		if i % 2 == 0:
 			filename = 'tmp_file' + str(i)
 			print_debug('EVEN ' + path + '/' + filename)
-			# current_file = open(path, 'w')
-			# current_file.write(path)
-			# current_file.close()
+			current_file = open(filename, 'w')
+			current_file.write(path + '/' + filename)
+			current_file.close()
 
 
 def create_files_odd(path, amount):
+	if not check_folder(path):
+		create_folder(path)
+		os.chdir(os.getcwd() + '/' + path)
 	for i in range(1, amount + 1):
 		if i % 2 != 0:
 			filename = 'tmp_file' + str(i)
 			print_debug('ODD ' + path + '/' + filename)
-			# current_file = open(path, 'w')
-			# current_file.write(path)
-			# current_file.close()
+			current_file = open(filename, 'w')
+			current_file.write(path + '/' + filename)
+			current_file.close()
 
 
-create_files_full('fill', 5)
+def check_folder(path):
+	if Path.exists(Path(path)):
+		return True
+	else:
+		return False
+
+
+def create_folder(path):
+	Path.mkdir(Path(path))
+
+
+def cleanup(path):
+	if check_folder(path):
+		shutil.rmtree(path)
+		return True
+	else:
+		return False
+
+
+#create_files_full('fill', 5)
 create_files_even('fill', 5)
-create_files_odd('fill', 5)
+#create_files_odd('fill', 5)
 
 
