48402bcc6d47d905777ac821383179d33350752a
Dallas Hall <dallas.hall@example.com>
2018-08-08 15:41:54 +1000

    Fixed the relative path bug. It was lolz. Refactored 3 functions into 1.

--- Automate The Boring Stuff/Chapter09-Organising Files/FillFileGaps.py
+++ Automate The Boring Stuff/Chapter09-Organising Files/FillFileGaps.py
@@ -1,53 +1,52 @@
-import os, shutil
+import os, shutil, random
 from pathlib import Path
 from PrintMessages import print_debug, print_info, print_error, print_warning
 
 
-def create_files_full(path, amount):
+def create_files(path, amount, no_filename_gaps):
+	print(path)
 	if not check_folder_exists(path):
+		if not Path.is_absolute(Path(path)):
+			path = get_absolute_path(path)
 		create_folder(path)
 		change_folder(path)
-	path = get_absolute_path(path)
-	print_info('Creating FULL files at:\n' + path)
+	else:
+		if not Path.is_absolute(Path(path)):
+			path = get_absolute_path(path)
+	print_info('create_files_even using\n' + str(path))
+	prn = random.randint(1, 100)
 	for i in range(1, amount + 1):
 		filename = 'tmp_file' + str(i)
-		print_debug('FULL' + path + '/' + filename)
-		current_file = open(filename, 'w')
-		current_file.write(path + '/' + filename)
-		current_file.close()
+		content = str(path) + '/' + filename
+
+		if no_filename_gaps:
+			print_debug('create_files - No filename gaps.')
+			print(content)
+			write_file(filename, content)
+		else:
+			if prn >= 50:
+				if i % 2 == 0:
+					print_debug('create_files - Odd filename gaps.')
+					print(content)
+					write_file(filename, content)
+			else:
+				if i % 2 != 0:
+					print_debug('create_files - Even filename gaps.')
+					print(content)
+					write_file(filename, content)
 
 
-def create_files_even(path, amount):
-	if not check_folder_exists(path):
-		create_folder(path)
-		change_folder(path)
-	path = get_absolute_path(path)
-	print_info('Creating EVEN files at:\n' + path)
-	for i in range(1, amount + 1):
-		if i % 2 == 0:
-			filename = 'tmp_file' + str(i)
-			print_debug('EVEN ' + path + '/' + filename)
-			current_file = open(filename, 'w')
-			current_file.write(path + '/' + filename)
-			current_file.close()
-
-
-def create_files_odd(path, amount):
-	if not check_folder_exists(path):
-		create_folder(path)
-		change_folder(path)
-	path = get_absolute_path(path)
-	print_info('Creating ODD files at:\n' + path)
-	for i in range(1, amount + 1):
-		if i % 2 != 0:
-			filename = 'tmp_file' + str(i)
-			print_debug('ODD ' + path + '/' + filename)
-			current_file = open(filename, 'w')
-			current_file.write(path + '/' + filename)
-			current_file.close()
+def write_file(filename, content):
+	current_file = open(filename, 'w')
+	current_file.write(str(content))
+	current_file.close()
 
 
 def check_folder_exists(path):
+	if not Path.is_absolute(Path(path)):
+		path = get_absolute_path(path)
+		print_debug('check_folder_exists - Converted to absolute path using')
+		print(path)
 	if Path.exists(Path(path)):
 		return True
 	else:
@@ -55,42 +54,83 @@ def check_folder_exists(path):
 
 
 def create_folder(path):
-	Path.mkdir(Path(path))
+	if Path.is_absolute(Path(path)):
+		Path.mkdir(Path(path))
+	else:
+		path = get_absolute_path(path)
+		Path.mkdir(path)
+	print_debug('create_folder using')
+	print(str(path))
 
 
 def change_folder(path):
 	if Path.is_absolute(Path(path)):
 		os.chdir(path)
 	else:
-		os.chdir(os.getcwd() + '/' + path)
+		path = get_absolute_path(path)
+		os.chdir(str(path))
+	print_debug('change_folder using')
+	print(str(path))
 
 
 def get_absolute_path(path):
 	if Path.is_absolute(Path(path)):
 		return path
 	else:
-		return os.getcwd() + '/' + path
+		print_debug('get_absolute_path - Returning\n' + str(Path.cwd() / path))
+		return runtime_path / path
 
 
 def cleanup(path):
 	if not Path.is_absolute(Path(path)):
-		print_error("Supplied path must be absolute.")
-		return False
+		path = get_absolute_path(path)
 	if check_folder_exists(path):
-		print_info('Deleting:\n' + path)
+		print_info('cleanup - Deleting' + str(path))
 		shutil.rmtree(path)
 		return True
 	else:
+		print_error('cleanup - Supplied path doesn\'t exist.')
+		print(path)
 		return False
 
 
+def fill_in_file_gaps_with_insert(path, number):
+	if not Path.is_absolute(Path(path)):
+		path = get_absolute_path(path)
+	if not (check_folder_exists(path)):
+		print_error('fill_in_file_gaps_with_insert - Supplied path doesn\'t exist.')
+		print(path)
+		return False
+	path = get_absolute_path(path)
+	files_found = []
+	for folderName, subfolders, files in os.walk(str(path)):
+		print('The current folder is ' + folderName)
+
+		for subfolder in subfolders:
+			print('There is a subfolder called ' + subfolder)
+
+		for file in files:
+			print('There is a file called ' + file)
+			files_found.append(file)
+
+	print(files_found)
+	files_found.sort()
+	print(files_found)
+
+def fill_in_file_gaps_with_reorder(path):
+	return None
+
+runtime_path = Path.cwd()
 relative_path = 'fill'
-absolute_path = os.getcwd() + '/' + relative_path
+absolute_path = get_absolute_path(relative_path)
 #create_files_full(absolute_path, 5)
-create_files_even(relative_path, 5)
-#create_files_odd(relative_path, 5)
+create_files(relative_path, 5, False)
+#create_files(absolute_path, 5, False)
+#create_files(absolute_path, 5, True)
+fill_in_file_gaps_with_insert(relative_path, 5)
+#fill_in_file_gaps_with_insert(absolute_path, 5)
 cleanup(relative_path)
-cleanup(absolute_path)
+#cleanup(absolute_path)
 
 
 
