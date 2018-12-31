2eee0802261b99e1c400ef419d04be0c65ccce59
Dallas Hall <mr.dallas.hall@gmail.com>
2018-12-31 11:22:42 +1100

    Reading cell value examples.

--- Automate The Boring Stuff/Chapter12-Spreadsheets/ReadCellValules.py
+++ Automate The Boring Stuff/Chapter12-Spreadsheets/ReadCellValules.py
@@ -6,19 +6,21 @@ workbook = load_workbook('../Examples/example.xlsx')
 
 # https://openpyxl.readthedocs.io/en/stable/usage.html#read-an-existing-workbook
 worksheet = workbook.active
+
+# Dates are automatically interpreted and returned as a datetime rather than a string.
 print('The value at ' + worksheet['A1'].coordinate + ' is')
 print(worksheet['A1'].value)
-print('The format at ' + worksheet['A1'].coordinate + ' is')
+print('The format at ' + str(worksheet['A1'].row) + str(worksheet['A1'].column) + ' is')
 print(worksheet['A1'].number_format)
 print()
 print('The value at ' + worksheet['B1'].coordinate + ' is')
 print(worksheet['B1'].value)
-print('The format at ' + worksheet['B1'].coordinate + ' is')
+print('The format at ' + str(worksheet['B1'].row) + str(worksheet['B1'].column) + ' is')
 print(worksheet['B1'].number_format)
 print()
 print('The value at ' + worksheet['C1'].coordinate + ' is')
 print(worksheet['C1'].value)
-print('The format at ' + worksheet['C1'].coordinate + ' is')
+print('The format at ' + str(worksheet['C1'].row) + str(worksheet['C1'].column) + ' is')
 print(worksheet['C1'].number_format)
 
 
