ef488a2e928611a68c10e274eca77999ef67dedd
Dallas Hall <dallas.hall@example.com>
2019-02-27 08:05:12 +1100

    Added index printing.

--- Automate The Boring Stuff/Chapter12-Spreadsheets/GetColumnsAndRows.py
+++ Automate The Boring Stuff/Chapter12-Spreadsheets/GetColumnsAndRows.py
@@ -14,15 +14,17 @@ logging.info('Starting GetColumnsAndRows.py')
 workbook = load_workbook('../Examples/example.xlsx')
 
 logging.debug('Opening Worksheet.')
-time.sleep(0.005)
+time.sleep(0.003)
 # https://openpyxl.readthedocs.io/en/stable/usage.html#read-an-existing-workbook
 #worksheet = workbook.active
 worksheet = workbook['Sheet1']
 if debugging:
-	time.sleep(0.005)
 	logging.info('The rows and cells returned are:')
+	time.sleep(0.003)
 	print(tuple(worksheet))
 
+logging.info('Printing row cells using tuple slices.')
+time.sleep(0.003)
 i = 1
 # Use splices to get a rectangle of cells.
 for row in worksheet['A1':'C3']:
@@ -31,6 +33,14 @@ for row in worksheet['A1':'C3']:
 		print(cell.coordinate, cell.value)
 	print('--- END OF ROW ----')
 	i += 1
+
+logging.info('Printing row cell using indices.')
+time.sleep(0.003)
+for i in range(1, len(tuple(worksheet))):
+	print('--- ROW ' + str(i) + ' ---')
+	for cell in worksheet[i]:
+		print(cell.coordinate, cell.value)
+	print('--- END OF ROW ----')
 
 time.sleep(0.003)
 logging.info('Script completed.')
