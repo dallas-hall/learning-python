4cfabed56e0ace9c4fc93ca12b77a43cf1d4d9c4
Dallas Hall <dallas.hall@example.com>
2019-03-01 11:41:36 +1100

    Dictionary implemented.

--- Hashing/FileHashingAndComparison.py
+++ Hashing/FileHashingAndComparison.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import logging, sys, os, time, re, hashlib, pprint
+import logging, sys, os, time, re, hashlib, pprint, json
 from pathlib import Path, PurePath
 
 
@@ -20,8 +20,8 @@ def check_answer(message):
 def get_input_path():
 	while True:
 		print('Enter the input path of the files to hash. Relative and canonical paths will be transformed into aboslute paths.')
-		answer = input()
-		result = check_answer('Input path is\n' + answer + '\nIs this correct?')
+		answer = get_absolute_path(input())
+		result = check_answer('Input path is\n' + str(answer) + '\nIs this correct?')
 		if result == 'y':
 			return answer
 
@@ -29,27 +29,10 @@ def get_input_path():
 def get_output_path():
 	while True:
 		print('Enter the output path of the files to hash. Relative and canonical paths will be transformed into aboslute paths.')
-		answer = input()
-		result = check_answer('Output path is\n' + answer + '\nIs this correct?')
+		answer = get_absolute_path(input())
+		result = check_answer('Output path is\n' + str(answer) + '\nIs this correct?')
 		if result == 'y':
 			return answer
-
-
-def get_absolute_path(path):
-	# check if a . or .. shortcut was given and make an absolute path if it was
-	match = re.match('^~', path)
-	if match:
-		return Path(path).expanduser()
-
-	match = re.match('^\\.', path)
-	if match:
-		return Path.resolve(Path(path))
-
-	match = re.match('^\\.\\.', path)
-	if match:
-		return Path.resolve(Path(path))
-
-	return Path.resolve(Path(path))
 
 
 def get_hashing_algorithm():
@@ -70,9 +53,73 @@ def is_valid_hash_algorithm(algorithm):
 	return algorithm.lower() in guaranteed_hash_algorithms
 
 
+def get_absolute_path(path):
+	# check if a . or .. shortcut was given and make an absolute path if it was
+	match = re.match('^~', path)
+	if match:
+		return Path(path).expanduser()
 
-# Define logging output
+	match = re.match('^\.|^\.\.', path)
+	if match:
+		return Path.resolve(Path(path))
+
+	return Path.resolve(Path(path))
+
+
+def get_all_files_absolute_paths(directory):
+	return [PurePath.joinpath(directory, file) for file in Path(directory).iterdir()]
+
+
+def hash_files(algorithm):
+	for file in Path(input_path).iterdir():
+		choices = {
+			'blake2b': hashlib.blake2b(Path(file).read_bytes()).hexdigest(),
+			'blake2s': hashlib.blake2s(Path(file).read_bytes()).hexdigest(),
+			'md5': hashlib.md5(Path(file).read_bytes()).hexdigest(),
+			'sha1': hashlib.sha1(Path(file).read_bytes()).hexdigest(),
+			'sha224': hashlib.sha224(Path(file).read_bytes()).hexdigest(),
+			'sha256': hashlib.sha256(Path(file).read_bytes()).hexdigest(),
+			'sha384': hashlib.sha384(Path(file).read_bytes()).hexdigest(),
+			'sha3_224': hashlib.sha3_224(Path(file).read_bytes()).hexdigest(),
+			'sha3_256': hashlib.sha3_256(Path(file).read_bytes()).hexdigest(),
+			'sha3_384': hashlib.sha3_384(Path(file).read_bytes()).hexdigest(),
+			'sha3_512': hashlib.sha3_512(Path(file).read_bytes()).hexdigest(),
+			'sha512': hashlib.sha512(Path(file).read_bytes()).hexdigest(),
+			'shake_128': hashlib.shake_128(Path(file).read_bytes()).hexdigest(128),
+			'shake_256': hashlib.shake_256(Path(file).read_bytes()).hexdigest(256)
+		}
+
+		algorithm_output = choices.get(hashing_algorithm, hashlib.md5(Path(file).read_bytes()).hexdigest())
+		hashed_files[file.name] = {
+			'checked': False,
+			'hash': algorithm_output,
+			'hashAlgorithm': algorithm,
+			'duplicateFiles': []
+		}
+
+		if debugging:
+			logging.debug(file)
+			logging.debug(file.name)
+			logging.debug(str(file) + ' is\n' + algorithm_output)
+			logging.debug(algorithm)
+
+
+def check_for_duplicate_hashes():
+	for this_file, this_metadata in hashed_files.items():
+		if not this_metadata['checked']:
+			this_metadata['checked'] = True
+			for that_file, that_metadata in hashed_files.items():
+				if not that_metadata['checked'] and this_metadata['hash'] == that_metadata['hash']:
+					this_metadata['duplicateFiles'].append(that_file)
+					that_metadata['duplicateFiles'].append(this_file)
+		if debugging:
+			logging.debug('file is ' + str(this_file))
+			logging.debug('metadata is ' + str(this_metadata))
+
+
+# Define logging and pretty print output
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
+pp = pprint.PrettyPrinter()
 
 # Enable debugging messages
 debugging = True
@@ -82,16 +129,20 @@ if not debugging:
 logging.info('Starting ' + os.path.relpath(sys.argv[0]))
 time.sleep(.005)
 
-# System guaranteed algorithms.
+# System guaranteed hashing algorithms.
 guaranteed_hash_algorithms = sorted(hashlib.algorithms_guaranteed)
 
-# Get user input for paths
-input_path = get_absolute_path(get_input_path())
-output_path = get_absolute_path(get_output_path())
+# Get user answers for paths and hashing algorithm
+input_path = get_input_path()
+output_path = get_output_path()
 hashing_algorithm = get_hashing_algorithm()
-
 if debugging:
-	#logging.debug('input_path is\n' + str(input_path))
-	#logging.debug('output_path is\n' + str(output_path))
+	time.sleep(.005)
+	logging.debug('input_path is\n' + str(input_path))
+	logging.debug('output_path is\n' + str(output_path))
 	logging.debug('hashing_algorithm is\n' + str(hashing_algorithm))
 
+# Hash all files in the base directory of the input_path (no recursive file walking)
+hashed_files = {}
+hash_files(hashing_algorithm)
+check_for_duplicate_hashes()
