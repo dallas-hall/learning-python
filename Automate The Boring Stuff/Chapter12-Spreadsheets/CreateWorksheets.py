5f3773b0e8c16016b2bbf1adbac53a9afd73d5ef
Dallas Hall <mr.dallas.hall@gmail.com>
2019-03-13 08:51:33 +1100

    Create new spreadsheet and worksheets in memory, then save to disk.

---
+++ Automate The Boring Stuff/Chapter12-Spreadsheets/CreateWorksheets.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import logging, time
+from datetime import datetime
+from pathlib import Path
+import openpyxl
+
+# Define logging
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - [%(levelname)s] - %(message)s')
+debugging = True
+if not debugging:
+	logging.disable(logging.DEBUG)
+logging.info('Starting CreateWorksheets.py')
+start_time = datetime.now()
+time.sleep(0.005)
+
+# https://openpyxl.readthedocs.io/en/stable/tutorial.html#loading-from-a-file
+workbook = openpyxl.Workbook()
+logging.info('Display all worksheet names. They are stored a list.')
+time.sleep(0.005)
+print(workbook.sheetnames)
+
+logging.info('Display currently active worksheet name. Stored as a string and the default is \'Sheet\'.')
+time.sleep(0.005)
+worksheet = workbook.active  # Default is 0
+print(worksheet.title)
+
+logging.info('Update current worksheet name.')
+time.sleep(0.005)
+worksheet.title = 'Sheet-One'
+print(worksheet.title)
+
+logging.info('Create a new worksheet.')
+time.sleep(0.005)
+workbook.create_sheet('Sheet-Two')
+worksheet2 = workbook['Sheet-Two']
+print(workbook.sheetnames)
+print(worksheet2.title)
+
+logging.info('Copying an existing worksheet to a new worksheet.')
+time.sleep(0.005)
+worksheet3 = workbook.copy_worksheet(worksheet)
+print(workbook.sheetnames)
+
+logging.info('Saving the spreadsheet. Will overwrite without warning')
+time.sleep(0.005)
+workbook.save(Path.cwd() / 'output' / 'new-spreadsheet-with-new-worksheets.xlsx')
+
+elapsed_time = datetime.now() - start_time
+time.sleep(0.005)
+logging.info('Script completed. The process took {} (hh:mm:ss.ms)'.format(elapsed_time))
