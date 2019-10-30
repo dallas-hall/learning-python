b29454bf33fc67e38ee1d7daeece7e8a33642b3d
Dallas Hall <dallas.hall@example.com>
2019-10-30 20:34:35 +1100

    Python doesn't have constants..

---
+++ Python-Crash-Course/Chapter02/constant-variables.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+MY_CONSTANT = "Python doesn't actually have constant variables, but programmers write variables with UPPERCASE names to signal constant-like values. These values won't change during the program."
+print(MY_CONSTANT)
