39a885238a1cc9ddfbbae2d6da7376805841a9ed
Dallas Hall <dallas.hall@example.com>
2019-10-30 18:14:38 +1100

    Some basic string methods.

---
+++ Python-Crash-Course/Chapter02/string_methods.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+name = 'ada lovelace'
+# Capitalise the first letter of each word, like in a title.
+print(name.title())
+
+name = "Ada Lovelace"
+print(name.lower())
+print(name.upper())
+
+first_name = 'ada'
+last_name = 'lovelace'
+# Using a variable's value inside a string
+full_name = f"{first_name} {last_name}"
+print(full_name.title())
