98b86fa69494d42055a30c458b8fbecb4cec116f
Dallas Hall <dallas.hall@example.com>
2019-10-30 18:11:02 +1100

    Some crappy whitespace functions.

---
+++ Python-Crash-Course/Chapter02/whitespace.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+print(" Python")
+print("\tPython")
+print("\nPython")
+
+# These only work temporarily
+message = " Python "
+print('"' + message + '"')
+print('"' + message.rstrip() + '"')
+print('"' + message.lstrip() + '"')
+print('"' + message.strip() + '"')
+print('"' + message + '"')
