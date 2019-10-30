0a67cb976836e341541604cffe2ac4f9eac0fc69
Dallas Hall <mr.dallas.hall@gmail.com>
2019-10-30 20:32:40 +1100

    Some int and float math, plus the cool 1_000_000 number formatting trick.

---
+++ Python-Crash-Course/Chapter02/numbers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# Integer math, division always yields a float
+print("2 ** 2 is " + str(2 ** 2))
+print("2 * 2 is " + str(2 * 2))
+print("2 / 2 is " + str(2 / 2))
+print("2 / 3 is " + str(2 / 3))
+print("2 // 3 is " + str(2 // 3))
+print("2 + 2 is " + str(2 + 2))
+print("2 - 2 is " + str(2 - 2))
+
+# Float math
+print("0.1 + 0.1 is " + str(0.1 + 0.1))
+print("0.2 + 0.2 is " + str(0.2 + 0.2))
+print("2 * 0.1 is " + str(2 * 0.1))
+print("2 * 0.2 is " + str(2 * 0.2))
+
+# Large numbers
+two_to_the_thirty_one = 2_147_483_648
+print(two_to_the_thirty_one)
