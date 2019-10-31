ddd1ce6a09eed67982e8ff89877aeb4bc7d91f96
Dallas Hall <mr.dallas.hall@gmail.com>
2019-10-31 20:51:33 +1100

    Added list comprehension syntax.

--- Python-Crash-Course/Chapter04/more-list-examples.c.py
+++ Python-Crash-Course/Chapter04/more-list-examples.c.py
@@ -10,18 +10,4 @@ if not debugging:
 	logging.disable(logging.DEBUG)
 # Print start message and delay slightly	
 logging.info('Starting ' + os.path.relpath(sys.argv[0]))
-time.sleep(.003)
-
-# 1 to 10, exclude 11
-squares = []
-for i in range(1, 11):
-	# i to the power of 2
-	square = i ** 2
-	squares.append(square)
-
-print("The square values from 1 to 10.")
-print(squares)
-
-print("List comprehension - combining for loops and element creation into one line.")
-squares = [i ** 2 for i in range(1, 11)]
-print(squares)
+time.sleep(.001)
