f264d1def0a5fad2345591faa5b4793bacbb9324
Dallas Hall <dallas.hall@example.com>
2019-10-31 19:52:03 +1100

    Creating squares with a for loop and storing in a list.

---
+++ Python-Crash-Course/Chapter04/squares-list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import logging, sys, os, time
+
+# Define logging output
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
+
+# Enable debugging messages
+debugging = True
+if not debugging:
+	logging.disable(logging.DEBUG)
+# Print start message and delay slightly	
+logging.info('Starting ' + os.path.relpath(sys.argv[0]))
+time.sleep(.001)
+
+# 1 to 10, exclude 11
+squares = []
+for i in range(1, 11):
+	# i to the power of 2
+	square = i ** 2
+	squares.append(square)
+
+print("The square values from 1 to 10.")
+print(squares)
