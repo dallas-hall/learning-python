c25687cc13d8b7d892e4da792ecd593be6e08cd0
Dallas Hall <dallas.hall@example.com>
2019-11-01 21:50:33 +1100

    List slice examples.

---
+++ Python-Crash-Course/Chapter04/list-slice-examples.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import logging, sys, os, time
+
+# Define logging output
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
+
+# Enable debugging messages
+debugging = True
+if not debugging:
+	logging.disable(logging.DEBUG)
+# Print start message and delay slightly	
+logging.info('Starting ' + os.path.relpath(sys.argv[0]))
+time.sleep(.001)
+
+# A list of 1 to 10
+digits = list(range(1, 11))
+print(digits)
+print("The min is " + str(min(digits)))
+print("The max is " + str(max(digits)))
+print("The sum is " + str(sum(digits)))
+
+print("Printing the first half and second half slices of the list.")
+# Must do integer division
+# Leaving out the first part of the slice starts from the beginning
+print(digits[:len(digits) // 2])
+# Leaving out the second part of the slice finishes at the end
+print(digits[len(digits) // 2:])
+print("Printing the last items.")
+print(digits[-3:])
+print("Print every second item of the first half of the list.")
+print(digits[:len(digits) // 2:2])
+print("Print every second item of the last half of the list.")
+print(digits[len(digits) // 2::2])
+
+print("Using a slice in a for loop")
+for n in digits[:len(digits) // 2]:
+	print(n)
