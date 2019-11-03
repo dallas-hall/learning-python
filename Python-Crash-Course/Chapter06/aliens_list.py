63cc19c7ce101dcc26f3cadcf8ca5d8de8003a0a
Dallas Hall <mr.dallas.hall@gmail.com>
2019-11-03 21:04:33 +1100

    Simple example showing dictionaries stored inside a list.

--- Python-Crash-Course/Chapter06/aliens_list.py
+++ Python-Crash-Course/Chapter06/aliens_list.py
@@ -11,3 +11,39 @@ if not debugging:
 # Print start message and delay slightly	
 logging.info('Starting ' + os.path.relpath(sys.argv[0]))
 time.sleep(.001)
+
+aliensList = []
+
+# Start at 1, exclude 7
+number = 1
+for alien in range(1, 7):
+	if number == 1:
+		new_alien = {
+			'colour': 'green',
+			'points': 5,
+			'speed': 'slow'
+		}
+		aliensList.append(new_alien)
+		number += 1
+	elif number == 2:
+		new_alien = {
+			'colour': 'yellow',
+			'points': 10,
+			'speed': 'medium'
+		}
+		number += 1
+		aliensList.append(new_alien)
+	else:
+		new_alien = {
+			'colour': 'red',
+			'points': 15,
+			'speed': 'fast'
+		}
+		number = 1
+		aliensList.append(new_alien)
+
+for i in range(len(aliensList)):
+	print("Alien " + str(i) + " is:")
+	print("Colour: " + aliensList[i].get('colour'))
+	print("Points: " + str(aliensList[i].get('points')))
+	print("Speed: " + aliensList[i].get('speed'))
