ab84b7305d318d0bcafdc4bea926391c18952c02
Dallas Hall <mr.dallas.hall@gmail.com>
2019-11-03 19:52:55 +1100

    Basic set example.

---
+++ Python-Crash-Course/Chapter06/sets.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import logging, sys, os, time
+
+# Define logging output
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
+
+# Enable debugging messages
+debugging = True
+if not debugging:
+	logging.disable(logging.DEBUG)
+# Print start message and delay slightly	
+logging.info('Starting ' + os.path.relpath(sys.argv[0]))
+time.sleep(.001)
+
+# A set is a collection where everything must be unique.
+print("Printing the original list.")
+aList = [1, 1, 2, 3, 4, 5, 5, 5, 6]
+print(aList)
+print("Printing the original list as a set.")
+aSet = set(aList)
+print(aSet)
+
+# Duplicate items are lost. Sets do not retain any specific order.
+aSet2 = {'a', 'b', 'c', 'c'}
+print(aSet2)
+print(sorted(aSet2))
