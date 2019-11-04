3497e973a27bd84209b7a40f6014c0db82ec4d00
Dallas Hall <mr.dallas.hall@gmail.com>
2019-11-04 21:35:41 +1100

    while loop exmaple.

--- Python-Crash-Course/Chapter07/while-loop.py
+++ Python-Crash-Course/Chapter07/while-loop.py
@@ -11,3 +11,16 @@ if not debugging:
 # Print start message and delay slightly	
 logging.info('Starting ' + os.path.relpath(sys.argv[0]))
 time.sleep(.001)
+
+reply = ""
+print("You must be taller than 120cm to go in this ride.")
+while True:
+	reply = input("How tall are you? ")
+	if reply.isdigit():
+		reply = int(reply)
+		break
+
+if int(reply) >= 120:
+	print(f"Great, {reply} cm is tall enough to ride.")
+else:
+	print(f"Sorry, {reply} cm isn't  tall enough to ride.")
