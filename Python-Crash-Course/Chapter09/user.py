cb44046bdbf8b7e2584dd25f934b6bc81b20b4bf
Dallas Hall <mr.dallas.hall@gmail.com>
2019-12-02 19:04:11 +1100

    A simple example showing how to make a user from a class.

--- Python-Crash-Course/Chapter09/user.py
+++ Python-Crash-Course/Chapter09/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import logging, sys, os, time
+from datetime import datetime
+from pytz import timezone
 
 # Define logging output
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
@@ -11,3 +13,44 @@ if not debugging:
 # Print start message and delay slightly	
 logging.info('Starting ' + os.path.relpath(sys.argv[0]))
 time.sleep(.001)
+
+class User:
+	# The constructor
+	def __init__(self, name, password, email):
+		self.name = name
+		self.password = password
+		self.email = email
+		self.join_date = datetime.now(timezone("Australia/Sydney")).strftime("%Y-%m-%d %H:%M:%S")
+
+	def get_name(self):
+		return self.name
+
+	def get_password(self):
+		return self.password
+
+	def set_password(self, password):
+		self.password = password
+
+	def get_email(self):
+		return self.email
+
+	def set_email(self, email):
+		self.email = email
+
+	def get_join_date(self):
+		return self.join_date
+
+
+users = [
+	User("Alice", "abc123", "alice@gmail.com"),
+	User("Bob", "password", "bob@gmail.com")
+]
+
+for user in users:
+	print(f"The current user's name is {user.name}.")
+	print(f"The current user's password is {user.password}. Yeah, we store passwords in plaintext, YOLO.")
+	print(f"The current user's email is {user.email}.")
+	print(f"The current user's join date is {user.join_date}.")
+
+
+
