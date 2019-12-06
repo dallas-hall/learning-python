95619c4bd79baeacc6dbcb455eacc3a51a09d7b9
Dallas Hall <mr.dallas.hall@gmail.com>
2019-12-06 21:58:36 +1100

    Subclass of user.py

--- Python-Crash-Course/Chapter09/admin.py
+++ Python-Crash-Course/Chapter09/admin.py
@@ -1,13 +1,10 @@
-#!/usr/bin/python3
-import logging, sys, os, time
+from user import User
+from privileges import Privileges
 
-# Define logging output
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
 
-# Enable debugging messages
-debugging = True
-if not debugging:
-	logging.disable(logging.DEBUG)
-# Print start message and delay slightly	
-logging.info('Starting ' + os.path.relpath(sys.argv[0]))
-time.sleep(.001)
+# Need to send the superclass as a parameter for the subclass
+class Admin(User):
+	def __init__(self, name, password, email, os):
+		super().__init__(name, password, email)
+		self.os_privileges = Privileges(os)
+
