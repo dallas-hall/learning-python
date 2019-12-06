43342c961f19c0101d67a0b24c49cf505da23331
Dallas Hall <dallas.hall@example.com>
2019-12-06 21:15:03 +1100

    Simple inheritance example, Car (as a module) and Electric car.

--- Python-Crash-Course/Chapter09/inheritance.py
+++ Python-Crash-Course/Chapter09/inheritance.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import logging, sys, os, time
+# To import this in PyCharm, right click the folder and mark as Sources Root
+from car import Car
 
 # Define logging output
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
@@ -12,29 +14,7 @@ if not debugging:
 logging.info('Starting ' + os.path.relpath(sys.argv[0]))
 time.sleep(.001)
 
-# Superclass must be in the same file and before the sub class(es)
-class Car:
-	# Constructor
-	def __init__(self, make, model, year):
-		self.make = make
-		self.model = model
-		self.year = year
-		self.odometer = 0
-
-	def get_details(self):
-		return f"{self.year} {self.make} {self.model} {self.odometer} km"
-
-	def read_odometer(self):
-		return f"{self.odometer}"
-
-	def update_odometer(self, kilometers):
-		if kilometers >= self.odometer:
-			self.odometer = kilometers
-		else:
-			print("Cannot roll back the odometer.")
-
-	def increment_odometer(self, kilometers):
-		self.odometer += kilometers
+# Car was moved into car.py to showcase modules. It could be defined here too.
 
 
 # Use inheritance to create a sub class
