a47a02275be8f1c738bbe1e25bbff7bd5798313a
Dallas Hall <mr.dallas.hall@gmail.com>
2019-12-06 21:58:14 +1100

    Superclass.

--- Python-Crash-Course/Chapter09/car.py
+++ Python-Crash-Course/Chapter09/car.py
@@ -1,17 +1,3 @@
-#!/usr/bin/python3
-import logging, sys, os, time
-
-# Define logging output
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
-
-# Enable debugging messages
-debugging = True
-if not debugging:
-	logging.disable(logging.DEBUG)
-# Print start message and delay slightly	
-logging.info('Starting ' + os.path.relpath(sys.argv[0]))
-time.sleep(.001)
-
 # Superclass must be in the same file and before the sub class(es)
 class Car:
 	# Constructor
