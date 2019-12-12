f37c3b8614d9e9384ec6756c3718c90d9e812844
Dallas Hall <mr.dallas.hall@gmail.com>
2019-12-12 21:45:25 +1100

    Added file metadata text file writing, which is a tab CSV. Has hashing algorithm used, the file hashes, and the associated files to those file hashes.

---
+++ Python-Crash-Course/Chapter10/replace-example.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import logging, sys, os, time
+
+# Define logging output
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
+
+# Enable debugging messages
+debugging = True
+if not debugging:
+	logging.disable(logging.DEBUG)
+# Print start message and delay slightly	
+logging.info('Starting ' + os.path.relpath(sys.argv[0]))
+time.sleep(.001)
+
+message = "I really like dogs!"
+print(message)
+message = message.replace("dog", "cat")
+print(message)
