46a2f0c89956107ad5f1eb20eb0ed08fe35753cd
Dallas Hall <mr.dallas.hall@gmail.com>
2019-12-12 22:09:02 +1100

    Create a User object and store it in a text file.

--- Python-Crash-Course/Chapter10/greet-user.py
+++ Python-Crash-Course/Chapter10/greet-user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import logging, sys, os, time
+from user import User
+from getpass import getpass
 
 # Define logging output
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
@@ -11,3 +13,18 @@ if not debugging:
 # Print start message and delay slightly	
 logging.info('Starting ' + os.path.relpath(sys.argv[0]))
 time.sleep(.001)
+
+username = input("Hello, please enter your username: ")
+password = getpass("Password: ")
+email = input("Email: ")
+
+new_user = User(username, password, email)
+
+absolute_file_path = "/media/veracrypt1/Development/io-files/users.txt"
+delimiter = ","
+with open(absolute_file_path, "a") as file_object:
+	file_object.write(new_user.get_name() + delimiter)
+	file_object.write(new_user.get_password() + delimiter)
+	file_object.write(new_user.get_email() + delimiter)
+	file_object.write(new_user.get_join_date() + "\n")
+
