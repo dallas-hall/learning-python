acefdebfd9338ef349eb525b27bdb97b5fd7040e
Dallas Hall <mr.dallas.hall@gmail.com>
2019-12-12 22:20:46 +1100

    Simple try/except/else example

---
+++ Python-Crash-Course/Chapter10/try-except-else.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import logging, sys, os, time
+
+# Define logging output
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
+
+# Enable debugging messages
+debugging = True
+if not debugging:
+	logging.disable(logging.DEBUG)
+# Print start message and delay slightly	
+logging.info('Starting ' + os.path.relpath(sys.argv[0]))
+time.sleep(.001)
+
+# Handle the exception object
+n = input("Enter an integer: ")
+m = input("Enter an integer: ")
+
+try:
+	answer = int(n) / int(m)
+except ZeroDivisionError:
+	print("Cannot divide by 0.")
+except ValueError:
+	print("You entered an invalid character.")
+else:
+	print(f"{n} / {m} is {answer}")
+
