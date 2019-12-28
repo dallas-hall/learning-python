3f080d61e5dcde4f92428634d4fb8a1339ba7ce4
Dallas Hall <dallas.hall@example.com>
2019-12-28 20:37:16 +1100

    Automating the testing by using setUp.

--- Python-Crash-Course/Chapter11/test_survey.py
+++ Python-Crash-Course/Chapter11/test_survey.py
@@ -18,23 +18,25 @@ time.sleep(.001)
 class TestAnonymousSurvey(unittest.TestCase):
 	"""Test the AnonymousSurvey class"""
 
+	# This is always run before every test_* is executed
+	def setUp(self):
+		# Create the AnonymousSurvey object
+		question = "What language did you first learn to speak?"
+		# Call the constructor
+		self.my_survey = AnonymousSurvey(question)
+		self.responses = ["English", "Korean", "Spanish"]
 
 	def test_store_single_response(self):
 		"""Test that a single response is stored correctly."""
-		question = "What language did you first learn to speak?"
-		my_survey = AnonymousSurvey(question)
-		my_survey.store_response("English")
-		self.assertIn("English", my_survey.responses)
+		self.my_survey.store_response(self.responses[0])
+		self.assertIn(self.responses[0], self.my_survey.responses)
 
 	def test_store_three_responses(self):
-		"""Test that a single response is stored correctly."""
-		question = "What language did you first learn to speak?"
-		my_survey = AnonymousSurvey(question)
-		responses = ["English", "Korean", "Spanish"]
-		for response in responses:
-			my_survey.store_response(response)
-		for response in responses:
-			self.assertIn(response, my_survey.responses)
+		"""Test that three responses are stored correctly."""
+		for response in self.responses:
+			self.my_survey.store_response(response)
+		for response in self.responses:
+			self.assertIn(response, self.my_survey.responses)
 
 	if __name__ == "__main__":
 		unittest.main()
