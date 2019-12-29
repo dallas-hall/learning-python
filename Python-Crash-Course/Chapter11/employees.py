1215c20cdce68f0715f24f94c2741484d9fb504e
Dallas Hall <dallas.hall@example.com>
2019-12-29 16:41:24 +1100

    Creating simple employee object

--- Python-Crash-Course/Chapter11/employees.py
+++ Python-Crash-Course/Chapter11/employees.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import logging, sys, os, time
+# To import this in PyCharm, right click the folder and mark as Sources Root
+from employee import Employee
 
 # Define logging output
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
@@ -11,3 +13,16 @@ if not debugging:
 # Print start message and delay slightly	
 logging.info('Starting ' + os.path.relpath(sys.argv[0]))
 time.sleep(.001)
+
+names = ["John Doe", "Jane Doe"]
+employess = []
+
+for name in names:
+	new_employee = Employee(name, "IT")
+	employess.append(new_employee)
+
+for employee in employess:
+	employee.print_details()
+
+
+
