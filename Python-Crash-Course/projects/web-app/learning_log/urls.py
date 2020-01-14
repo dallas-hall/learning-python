3dee83643b9a1861a46d7a85d0c8cbd07f1757c1
Dallas Hall <mr.dallas.hall@gmail.com>
2020-01-14 12:47:24 +1100

    Replace the default Django page with a new custome home page.

--- Python-Crash-Course/projects/web-app/learning_log/urls.py
+++ Python-Crash-Course/projects/web-app/learning_log/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Adding new urls to the site
+    path("", include("learning_logs.urls")),  # We needed to create this file.
 ]
