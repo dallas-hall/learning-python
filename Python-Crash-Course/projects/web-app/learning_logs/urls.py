3cea06e660a4628bf389fa095c6aa28af73b64c4
Dallas Hall <mr.dallas.hall@gmail.com>
2020-01-14 13:12:31 +1100

    Replaced default Django home page with a custom home page.

---
+++ Python-Crash-Course/projects/web-app/learning_logs/urls.py
@@ -0,0 +1,18 @@
+"""
+Defines URL patterns for learning_logs app.
+"""
+# Maps URLs to views
+from django.urls import path
+# import the views from the current directory (i.e. dot)
+from . import views
+
+app_name = "learning_logs"
+
+# The pages that can be requested by this app.
+urlpatterns = [
+    # Home Page
+    # 1st arg = Routing string, ignores base URL (e.g. http://localhost:8000)
+    # 2nd arg = What function to call
+    # 3rd arg = URL pattern name, used internally as a reference.
+    path("", views.index, name="index"),
+]
