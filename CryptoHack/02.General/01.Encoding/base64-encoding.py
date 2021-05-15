e6e8f13e39bc90e078c0cad3b70df1e3e0282a92
Dallas Hall <dallas.hall@example.com>
2021-05-15 20:27:09 +1000

    Added in logging and removed tabs.

--- CryptoHack/02.General/01.Encoding/base64-encoding.py
+++ CryptoHack/02.General/01.Encoding/base64-encoding.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
+# THIRD CHALLENGE
+
 # https://docs.python.org/3/library/base64.html#module-base64
 import base64
 # https://docs.python.org/3/library/binascii.html
 import binascii
+import logging
+import os
+import sys
+import time
 
-# Another common encoding scheme is Base64, which allows us to represent binary data as an ASCII string using 64 characters. One character of a Base64 string encodes 6 bits, and so 4 characters of Base64 encodes three 8-bit bytes.
+# Define logging output
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
+
+# Enable debugging messages
+debugging = True
+if not debugging:
+	logging.disable(logging.DEBUG)
+# Print start message and delay slightly
+logging.info('Starting ' + os.path.relpath(sys.argv[0]))
+time.sleep(.001)
+
+# Another common encoding scheme is Base64, which allows us to represent binary data as an ASCII string using 64 characters.
+# One character of a Base64 string encodes 6 bits, and so 4 characters of Base64 encodes three 8-bit bytes.
 # Base64 is most commonly used online, where binary data such as images can be easy included into html or css files.
 # Take the below hex string, decode it into bytes and then encode it into Base64.
 hex_str = '72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf'
@@ -15,8 +33,7 @@ binary_str = binascii.unhexlify(hex_str)
 print(binary_str)
 
 # https://docs.python.org/3/library/base64.html#base64.b64decode
-binary_flag = base64.b64encode(binary_str) 
+binary_flag = base64.b64encode(binary_str)
 flag = binary_flag.decode()
 
 print(flag)
-
--- CryptoHack/02.General/01.Encoding/hex-encoding.py
+++ CryptoHack/02.General/01.Encoding/hex-encoding.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
 
+# SECOND CHALLENGE
+
 # https://docs.python.org/3/library/binascii.html
 import binascii
+import logging
+import os
+import sys
+import time
 
-# When we encrypt something the resulting ciphertext commonly has bytes which are not printable ASCII characters. If we want to share our encrypted data, it's common to encode it into something more user-friendly and portable across different systems.
+# Define logging output
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - [%(levelname)s] - %(message)s')
+
+# Enable debugging messages
+debugging = True
+if not debugging:
+    logging.disable(logging.DEBUG)
+# Print start message and delay slightly
+logging.info('Starting ' + os.path.relpath(sys.argv[0]))
+time.sleep(.001)
+
+# When we encrypt something the resulting ciphertext commonly has bytes which are not printable ASCII characters.
+# If we want to share our encrypted data, it's common to encode it into something more user-friendly and portable across different systems.
 # Included below is a flag encoded as a hex string. Decode this back into bytes to get the flag.
 hex_str = '63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d'
 
 # https://docs.python.org/3/library/binascii.html#binascii.unhexlify
 flag_binary = binascii.unhexlify(hex_str)
-print (flag_binary)
+print(flag_binary)
 
 # https://docs.python.org/3/library/stdtypes.html#bytes.decode
 flag = flag_binary.decode()
 print(flag)
-
