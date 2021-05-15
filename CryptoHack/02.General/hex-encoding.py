5d90051d551e8f30b16e1d780e4c9197167158de
Dallas Hall <dallas.hall@example.com>
2021-05-15 18:29:19 +1000

    Added in some notes for the challenges.

--- CryptoHack/02.General/hex-encoding.py
+++ CryptoHack/02.General/hex-encoding.py
@@ -3,7 +3,10 @@
 # https://docs.python.org/3/library/binascii.html
 import binascii
 
+# When we encrypt something the resulting ciphertext commonly has bytes which are not printable ASCII characters. If we want to share our encrypted data, it's common to encode it into something more user-friendly and portable across different systems.
+# Included below is a flag encoded as a hex string. Decode this back into bytes to get the flag.
 hex_str = '63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d'
+
 # https://docs.python.org/3/library/binascii.html#binascii.unhexlify
 flag_binary = binascii.unhexlify(hex_str)
 print (flag_binary)
